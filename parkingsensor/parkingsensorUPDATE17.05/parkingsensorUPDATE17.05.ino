79a4d1139ce47dab543bce7c08245bc84f9e7a24
Stefank996 <stefankaraga021@gmail.com>
2024-05-27 22:41:53 +0200

    'v1.0'

--- parkingsensor/parkingsensorUPDATE17.05/parkingsensorUPDATE17.05.ino
+++ parkingsensor/parkingsensorUPDATE17.05/parkingsensorUPDATE17.05.ino
@@ -1,14 +1,14 @@
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h> // includes the LiquidCrystal Library 
+#include <LiquidCrystal_I2C.h>  // includes the LiquidCrystal Library
 
-const int switchPin = 2; // Slide Switch (prekidac)
+const int switchPin = 2;  // Slide Switch (prekidac)
 
 #define trigPin 13
 #define echoPin 12
 //sonar define
 
-#define GREEN 4
-#define RED 6
+#define GREEN 6
+#define RED 4
 //LED define
 
 #define buzzerPin 8
@@ -16,32 +16,36 @@ const int switchPin = 2; // Slide Switch (prekidac)
 
 #define ventilatorPin 7
 
+bool fanOn = false;
+bool delayActive = false;
+unsigned long previousMillis = 0;
+unsigned long delayTime = 3000;  // Odlaganje od 3 sekunde
 
-LiquidCrystal_I2C lcd(0x3f,16,2); 
+LiquidCrystal_I2C lcd(0x3f, 16, 2);
 String message1 = "Projekat:";
 String message2 = "Parking senzor";
 String message3 = "Rikverc";
 String message4 = "za aktivaciju!";
 
-int switchState = 0; 
+int switchState = 0;
 
 
-void blink(int pin, int duration) { // Blink function
-  digitalWrite(pin, HIGH); //Turn on LED
-  delay(duration); // LED ON for the specified duration
-  digitalWrite(pin, LOW); //Turn off LED
-  delay(duration); // LED OFF for the specified duration
+void blink(int pin, int duration) {  // Blink function
+  digitalWrite(pin, HIGH);           //Turn on LED
+  delay(duration);                   // LED ON for the specified duration
+  digitalWrite(pin, LOW);            //Turn off LED
+  delay(duration);                   // LED OFF for the specified duration
 }
 
 void buzz(int duration) {
-  digitalWrite(buzzerPin, HIGH); // Turn on buzzer
-  delay(duration); // Wait for the specified duration
-  digitalWrite(buzzerPin, LOW); // Turn off buzzer
+  digitalWrite(buzzerPin, HIGH);  // Turn on buzzer
+  delay(duration);                // Wait for the specified duration
+  digitalWrite(buzzerPin, LOW);   // Turn off buzzer
   delay(duration);
 }
 
-void setup() { 
-  
+void setup() {
+
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -53,15 +57,15 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.display();
-  for(int i = 0; i < message1.length(); i++){
+  for (int i = 0; i < message1.length(); i++) {
     lcd.print(message1.charAt(i));
-    delay(200); 
+    delay(200);
   }
 
   lcd.blink();
-  lcd.setCursor(0,1);
-  
-  for(int i = 0; i < message2.length(); i++){
+  lcd.setCursor(0, 1);
+
+  for (int i = 0; i < message2.length(); i++) {
     lcd.print(message2.charAt(i));
     delay(200);
   }
@@ -79,76 +83,120 @@ int call_distance() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   long duration = pulseIn(echoPin, HIGH);
-  int distance = (duration / 2) / 29.1; // Calculate distance in centimeters
+  int distance = (duration / 2) / 29.1;  // Calculate distance in centimeters
   return distance;
 }
 
-void loop() {  
+void loop() {
 
   int distance = call_distance();
   Serial.print("Distance (cm): ");
   Serial.println(distance);
   switchState = digitalRead(switchPin);
-  
-  
+
+  unsigned long currentMillis = millis();
+
   if (switchState == LOW) {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(message3);
     lcd.setCursor(0, 1);
     lcd.print(message4);
-    delay(3000); 
+    delay(3000);
 
   } else {
     lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("Udaljenost: ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(distance);
     lcd.print(" cm");
 
-
- // odavde ide provera distance
     if (distance <= 3) {
-      digitalWrite(RED, HIGH); // Turn on RED LED
-      digitalWrite(GREEN, LOW); // Turn off GREEN LED
-      digitalWrite(buzzerPin, HIGH);
-      digitalWrite(ventilatorPin, HIGH);
-      
-  
-    } else if (distance > 3 && distance <= 8) {
-      digitalWrite(RED, HIGH);
-      digitalWrite(GREEN, LOW);
-      blink(RED, 40); // Blink the RED LED
-      buzz(50);
-       digitalWrite(ventilatorPin, LOW); // Buzz the buzzer
-    } else if (distance > 8 && distance <= 15) {
-      digitalWrite(RED, HIGH);
-      digitalWrite(GREEN, LOW);
-      blink(RED, 100);
-      buzz(100);
-       digitalWrite(ventilatorPin, LOW);
-    } else if (distance > 15 && distance <= 30) {
-      digitalWrite(GREEN, HIGH); // Turn on GREEN LED
-      digitalWrite(RED, LOW); // Turn off RED LED
-      blink(GREEN, 130); // Blink the GREEN LED
-      buzz(200);
-       digitalWrite(ventilatorPin, LOW);
-    } else if (distance > 30 && distance <= 50) {
-      digitalWrite(GREEN, HIGH);
-      digitalWrite(RED, LOW);
-      blink(GREEN, 150);
-      buzz(300);
-       digitalWrite(ventilatorPin, LOW);
-    } else {
-      digitalWrite(GREEN, HIGH); // Turn off GREEN LED
-      digitalWrite(RED, LOW); // Turn off RED LED
-      digitalWrite(buzzerPin, LOW); // Turn off Buzzer
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Vise od 50cm!");
-       digitalWrite(ventilatorPin, LOW);
-    }
+  digitalWrite(RED, HIGH);   // Turn on RED LED
+  digitalWrite(GREEN, LOW);  // Turn off GREEN LED
+  digitalWrite(buzzerPin, HIGH);
+  digitalWrite(ventilatorPin, HIGH);
+  fanOn = true;
+  delayActive = false;  // Resetujemo stanje odlaganja
+
+} else if (distance > 3 && distance <= 8) {
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  blink(RED, 40);  // Blink the RED LED
+  buzz(50);
+  if (fanOn && !delayActive) {
+    previousMillis = millis();
+    delayActive = true;
   }
-  delay(100); // ovaj delay mora postojati u izvrsavanju
+  if (delayActive && (millis() - previousMillis >= delayTime)) {
+    digitalWrite(ventilatorPin, LOW);
+    fanOn = false;
+    delayActive = false;
+  }
+
+} else if (distance > 8 && distance <= 15) {
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  blink(RED, 100);
+  buzz(100);
+  if (fanOn && !delayActive) {
+    previousMillis = millis();
+    delayActive = true;
+  }
+  if (delayActive && (millis() - previousMillis >= delayTime)) {
+    digitalWrite(ventilatorPin, LOW);
+    fanOn = false;
+    delayActive = false;
+  }
+
+} else if (distance > 15 && distance <= 30) {
+  digitalWrite(GREEN, HIGH);  // Turn on GREEN LED
+  digitalWrite(RED, LOW);     // Turn off RED LED
+  blink(GREEN, 130);          // Blink the GREEN LED
+  buzz(200);
+  if (fanOn && !delayActive) {
+    previousMillis = millis();
+    delayActive = true;
+  }
+  if (delayActive && (millis() - previousMillis >= delayTime)) {
+    digitalWrite(ventilatorPin, LOW);
+    fanOn = false;
+    delayActive = false;
+  }
+
+} else if (distance > 30 && distance <= 50) {
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(RED, LOW);
+  blink(GREEN, 150);
+  buzz(300);
+  if (fanOn && !delayActive) {
+    previousMillis = millis();
+    delayActive = true;
+  }
+  if (delayActive && (millis() - previousMillis >= delayTime)) {
+    digitalWrite(ventilatorPin, LOW);
+    fanOn = false;
+    delayActive = false;
+  }
+
+} else {
+  digitalWrite(GREEN, HIGH);     // Turn off GREEN LED
+  digitalWrite(RED, LOW);        // Turn off RED LED
+  digitalWrite(buzzerPin, LOW);  // Turn off Buzzer
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Vise od 50cm!");
+  if (fanOn && !delayActive) {
+    previousMillis = millis();
+    delayActive = true;
+  }
+  if (delayActive && (millis() - previousMillis >= delayTime)) {
+    digitalWrite(ventilatorPin, LOW);
+    fanOn = false;
+    delayActive = false;
+  }
+}
+  delay(100);  // Ovaj delay mora postojati u izvršavanju
+  }
 }
